8eafe945dcb46dc1ee54c3315894d65af6f5a2b5
axgn <axgn123@qq.com>
2026-03-26 16:20:38 +0800

    1234

--- leetcode_go/3548/main.go
+++ leetcode_go/3548/main.go
@@ -1,5 +1,14 @@
 package main
 
 func canPartitionGrid(grid [][]int) bool {
-	
+	m, n := len(grid), len(grid[0])
+	rowsum := make([]int, m)
+	colsum := make([]int, n)
+	for i, row := range grid {
+		for j, v := range row {
+			rowsum[i] += v
+			colsum[j] += v
+		}
+	}
+	return true
 }
