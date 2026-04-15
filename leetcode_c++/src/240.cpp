d4f1fffd89e08829194a0ca348d29755d1c0cb3d
axgn <3451962304@qq.com>
2026-04-15 13:38:25 +0800

    12344

---
+++ leetcode_c++/src/240.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  bool searchMatrix(vector<vector<int>> &matrix, int target) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int i = 0, j = n - 1;
+    while (i < m && j >= 0) {
+      if (target > matrix[i][j]) {
+        i++;
+      } else if (target < matrix[i][j]) {
+        j--;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+int main() {
+  Solution s;
+  cout << "" << "\n";
+  return 0;
+}
