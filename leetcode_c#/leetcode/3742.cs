c7f2c86c8696299f003922e6db9ba8fa8005930b
axgn <3451962304@qq.com>
2026-05-01 18:48:04 +0800

    1345

---
+++ leetcode_c#/leetcode/3742.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+  public int MaxPathScore(int[][] grid, int k)
+  {
+    int m = grid.Length, n = grid[0].Length;
+    int[][][] dp = new int[m][][];
+    for (int i = 0; i < m; i++)
+    {
+      dp[m] = new int[n][];
+      for (int i = 0; i < length; i++)
+      {
+        
+      }
+    }
+    return 0;
+  }
+}
