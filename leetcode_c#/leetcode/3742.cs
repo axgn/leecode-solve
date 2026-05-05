72e86642526eb1de929272fd5e677fffb219b1b9
axgn <axgn123@qq.com>
2026-05-05 23:57:52 +0800

    12344

--- leetcode_c#/leetcode/3742.cs
+++ leetcode_c#/leetcode/3742.cs
@@ -1,3 +1,5 @@
+Solution s = new Solution();
+Console.WriteLine(s.MaxPathScore([[0, 1], [2, 0]], 1));
 public class Solution
 {
   public int MaxPathScore(int[][] grid, int k)
@@ -6,13 +8,62 @@ public class Solution
     int[][][] dp = new int[m][][];
     for (int i = 0; i < m; i++)
     {
-      dp[m] = new int[n][];
+      dp[i] = new int[n][];
       for (int j = 0; j < n; j++)
       {
-        dp[m][n] = new int[k];
+        dp[i][j] = new int[k + 1];
       }
     }
-    
-    return 0;
+    dp[0][0][0] = 0;
+    for (int i = 1; i <= k; i++)
+    {
+      dp[0][0][i] = grid[0][0];
+    }
+    for (int i = 1; i < m; i++)
+    {
+      for (int j = 1; j <= k; j++)
+      {
+        if (grid[i][0] != 0)
+        {
+          dp[i][0][j] = dp[i - 1][0][j - 1] + grid[i][0];
+        }
+        else
+        {
+          dp[i][0][j] = dp[i - 1][0][j];
+        }
+      }
+    }
+    for (int i = 1; i < n; i++)
+    {
+      for (int j = 1; j <= k; j++)
+      {
+        if (grid[0][i] != 0)
+        {
+          dp[0][i][j] = dp[0][i - 1][j - 1] + grid[0][i];
+        }
+        else
+        {
+          dp[0][i][j] = dp[0][i - 1][j];
+        }
+      }
+    }
+    for (int i = 1; i < m; i++)
+    {
+      for (int j = 1; j < n; j++)
+      {
+        for (int l = 1; l <= k; l++)
+        {
+          if (grid[i][j] != 0)
+          {
+            dp[i][j][l] = Math.Max(dp[i - 1][j][l - 1], dp[i][j - 1][l - 1]) + grid[i][j];
+          }
+          else
+          {
+            dp[i][j][l] = Math.Max(dp[i][j - 1][l], dp[i - 1][j][l]);
+          }
+        }
+      }
+    }
+    return dp[m - 1][n - 1][k];
   }
 }
