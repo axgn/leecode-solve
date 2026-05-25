29a3b18c7774a2698ba56d8a7b2cfea61a55d671
axgn <axgn123@qq.com>
2026-05-25 21:15:36 +0800

    1223

--- leetcode_c++/src/1871.cpp
+++ leetcode_c++/src/1871.cpp
@@ -6,19 +6,22 @@
 #include <vector>
 
 using namespace std;
-class Solution {
+class Solution1 {
 public:
   bool canReach(string s, int minJump, int maxJump) {
     int n = s.length();
     vector<int8_t> vis(n);
     function<bool(int)> dfs = [&](int i) {
-      bool res = true;
-      for (int j = i + minJump; j <= i + maxJump; j++) {
-        if (vis[j] != 0) {
+      bool res = false;
+      vis[i] = 1;
+      for (int j = i + minJump; j <= min(n - 1, i + maxJump); j++) {
+        if (vis[j] != 0 || s[j] != '0') {
           continue;
         }
-        vis[j] = 1;
-        dfs(j);
+        if (j == n - 1) {
+          return true;
+        }
+        res |= dfs(j);
       }
       return res;
     };
@@ -26,8 +29,27 @@ public:
   }
 };
 
+class Solution {
+public:
+  bool canReach(string s, int minJump, int maxJump) {
+    int n = s.size();
+    vector<int8_t> f(n);
+    vector<int> sum(n + 1);
+    f[0] = true;
+    sum[1] = 1;
+
+    for (int j = 1; j < n; j++) {
+      f[j] = j >= minJump && s[j] == '0' &&
+             sum[j - minJump + 1] > sum[max(j - maxJump, 0)];
+      sum[j + 1] = sum[j] + f[j];
+    }
+
+    return f[n - 1];
+  }
+};
+
 int main() {
   Solution s;
-  cout << "" << "\n";
+  cout << s.canReach("01101110", 2, 3) << "\n";
   return 0;
 }
