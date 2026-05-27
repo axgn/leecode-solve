322393f181299d22e0084fa5b66da58dccbae84a
axgn <3451962304@qq.com>
2026-05-27 15:08:44 +0800

    1234

---
+++ leetcode_c++/src/3121.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  int numberOfSpecialChars(string word) {}
+};
+int main() {
+  Solution s;
+  cout << "" << "\n";
+  return 0;
+}
