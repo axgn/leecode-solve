1623b5de83a18856aec1aa79940d741a1d661e7f
axgn <3451962304@qq.com>
2026-06-14 18:43:17 +0800

    12333

--- leetcode_c++/src/2130.cpp
+++ leetcode_c++/src/2130.cpp
@@ -30,6 +30,49 @@ public:
   }
 };
 
+class Solution2 {
+public:
+  ListNode *findmidnode(ListNode *head) {
+    ListNode *slow = head, *fast = head;
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    return slow;
+  }
+  ListNode *reverselist(ListNode *head) {
+    ListNode *temp = nullptr;
+    if (!head) {
+      return nullptr;
+    }
+    while (head->next) {
+      ListNode *temp1 = head->next;
+      head->next = temp;
+      temp = head;
+      head = temp1;
+    }
+    return head;
+  }
+  int pairSum(ListNode *head) {
+    int cnt = 0;
+    ListNode *temp = head;
+    vector<int> v(100000);
+    while (temp) {
+      v[cnt] = temp->val;
+      temp = temp->next;
+      cnt++;
+    }
+    int max = INT_MIN;
+    for (int i = 0; i < cnt / 2; i++) {
+      int sum = v[i] + v[cnt - i - 1];
+      if (sum > max) {
+        max = sum;
+      }
+    }
+    return max;
+  }
+};
+
 int main() {
   Solution s;
   cout << "" << "\n";
