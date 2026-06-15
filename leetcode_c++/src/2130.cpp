c5f582fca4bdede9bc492ef2479568eae029021f
axgn <axgn123@qq.com>
2026-06-15 13:01:13 +0800

    12344

--- leetcode_c++/src/2130.cpp
+++ leetcode_c++/src/2130.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class Solution {
+class Solution2 {
 public:
   int pairSum(ListNode *head) {
     int cnt = 0;
@@ -30,7 +30,7 @@ public:
   }
 };
 
-class Solution2 {
+class Solution {
 public:
   ListNode *findmidnode(ListNode *head) {
     ListNode *slow = head, *fast = head;
@@ -42,32 +42,85 @@ public:
   }
   ListNode *reverselist(ListNode *head) {
     ListNode *temp = nullptr;
-    if (!head) {
-      return nullptr;
-    }
-    while (head->next) {
+    while (head) {
       ListNode *temp1 = head->next;
       head->next = temp;
       temp = head;
       head = temp1;
     }
-    return head;
+    return temp;
   }
-  int pairSum(ListNode *head) {
+  ListNode *reverselist(ListNode *head, int left, int right) {
     int cnt = 0;
-    ListNode *temp = head;
-    vector<int> v(100000);
-    while (temp) {
-      v[cnt] = temp->val;
-      temp = temp->next;
+    ListNode *dummy = new ListNode(0, head);
+    ListNode *cur = dummy;
+    while (cur && cnt < left - 1) {
+      cnt++;
+      cur = cur->next;
+    }
+    ListNode *parent = cur;
+    cur = cur->next;
+    ListNode *temp = cur;
+    cnt++;
+    ListNode *prev = nullptr;
+    while (cur && cnt < right - 1) {
+      ListNode *next = cur->next;
+      cur->next = prev;
+      prev = cur;
+      cur = next;
       cnt++;
     }
+    temp->next = cur;
+    parent->next = prev;
+    return dummy->next;
+  }
+  ListNode *reverseBetween(ListNode *head, int left, int right) {
+    int cnt = 0;
+    ListNode *dummy = new ListNode(0, head);
+    ListNode *cur = dummy;
+    while (cur && cnt < left - 1) {
+      cnt++;
+      cur = cur->next;
+    }
+    ListNode *parent = cur;
+    cur = cur->next;
+    ListNode *temp = cur;
+    cnt++;
+    ListNode *prev = nullptr;
+    while (cur && cnt <= right) {
+      ListNode *next = cur->next;
+      cur->next = prev;
+      prev = cur;
+      cur = next;
+      cnt++;
+    }
+    temp->next = cur;
+    parent->next = prev;
+    return dummy->next;
+  }
+  ListNode *reverselist(ListNode *head, ListNode *left, ListNode *right) {
+    ListNode *temp = nullptr;
+    while (head) {
+      ListNode *temp1 = head->next;
+      head->next = temp;
+      temp = head;
+      head = temp1;
+    }
+    return temp;
+  }
+
+  int pairSum(ListNode *head) {
     int max = INT_MIN;
-    for (int i = 0; i < cnt / 2; i++) {
-      int sum = v[i] + v[cnt - i - 1];
-      if (sum > max) {
-        max = sum;
+    ListNode *mid = findmidnode(head);
+    ListNode *node1 = reverselist(mid);
+    ListNode *node2 = head;
+    while (node1) {
+      int val = node1->val + node2->val;
+      if (val > max) {
+        max = val;
       }
+      node1 = node1->next;
+      node2 = node2->next;
     }
     return max;
   }
@@ -75,6 +128,9 @@ public:
 
 int main() {
   Solution s;
+  ListNode head(
+      1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+  s.reverseBetween(&head, 2, 4);
   cout << "" << "\n";
   return 0;
 }
