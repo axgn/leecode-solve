071ba26d3ede32867717891ecdaef4fe21d14996
axgn <3451962304@qq.com>
2026-07-04 19:14:40 +0800

    12344

---
+++ leetcode_c++/src/2492.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        
+    }
+};
+int main() {
+  Solution s;
+  cout << "" << "\n";
+  return 0;
+}
