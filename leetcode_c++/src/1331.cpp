ff7c5c119a23d3105a6029270cb427d852bee2d5
axgn <3451962304@qq.com>
2026-07-12 18:49:35 +0800

    12334

--- leetcode_c++/src/1331.cpp
+++ leetcode_c++/src/1331.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -10,7 +11,14 @@ public:
     vector<int> temp = arr;
     sort(temp.begin(), temp.end());
     temp.erase(unique(temp.begin(), temp.end()), temp.end());
-    
+    unordered_map<int, int> pos;
+    for (int i = 0; i < temp.size(); i++) {
+      pos[temp[i]] = i;
+    }
+    for (auto &i : arr) {
+      i = pos[i] + 1;
+    }
+    return arr;
   }
 };
 int main() {
