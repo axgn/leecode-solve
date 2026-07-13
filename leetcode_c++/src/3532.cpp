4fb7ecf8caed2611351ef827dd56f7e06b42ae3e
axgn <axgn123@qq.com>
2026-07-13 16:16:58 +0800

    12344

---
+++ leetcode_c++/src/3532.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<bool> pathExistenceQueries(int n, vector<int> &nums, int maxDiff,
+                                    vector<vector<int>> &queries) {
+  }
+};
+int main() {
+  Solution s;
+  cout << "" << "\n";
+  return 0;
+}
