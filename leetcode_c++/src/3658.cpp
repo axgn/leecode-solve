1efcd38bd64caa28ea2d441cc8b7b0212f8143fa
axgn <3451962304@qq.com>
2026-07-22 18:17:11 +0800

    122333

--- leetcode_c++/src/3658.cpp
+++ leetcode_c++/src/3658.cpp
@@ -9,17 +9,21 @@ public:
   int gcdOfOddEvenSums(int n) {
     int temp = 2 * n;
     int sum = temp * (temp + 1) / 2;
-    int odd_sum = (sum - 4) / 2;
-    int even_sum = odd_sum + 4;
-    auto gcd = [&]() {
-      while (even_sum / odd_sum) {
-        int temp = even_sum % odd_sum;
+    int odd_sum = (sum - n) / 2;
+    int even_sum = odd_sum + n;
+    auto gcd = [&](int a, int b) {
+      while (a % b) {
+        int temp = a % b;
+        a = b;
+        b = temp;
       }
+      return b;
     };
+    return gcd(odd_sum, even_sum);
   }
 };
 int main() {
   Solution s;
-  cout << "" << "\n";
+  cout << s.gcdOfOddEvenSums(5) << "\n";
   return 0;
 }
