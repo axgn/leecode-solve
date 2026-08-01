73ade5f654629ed3f118d75e4bca8ebc3cede7f2
axgn <axgn123@qq.com>
2026-08-01 13:39:39 +0800

    12344

--- leetcode_c++/src/3016.cpp
+++ leetcode_c++/src/3016.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <iterator>
 #include <vector>
@@ -8,10 +9,18 @@ class Solution {
 public:
   int minimumPushes(string word) {
     int cnt[26] = {0};
-    for (auto& ch : word) {
+    for (auto &ch : word) {
       cnt[ch - 'a']++;
     }
-    
+    ranges::sort(cnt, greater());
+    int res = 0;
+    for (int i = 0; i < 26; i++) {
+      if (cnt[i] == 0) {
+        continue;
+      }
+      res += cnt[i] * (i / 8 + 1);
+    }
+    return res;
   }
 };
 int main() {
---
+++ leetcode_c++/src/486.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  bool predictTheWinner(vector<int> &nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n, vector<int>(n));
+    for (int i = n - 1; i >= 0; i--) {
+      dp[i][i] = nums[i];
+      for (int j = i + 1; j < n; j++) {
+        dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+      }
+    }
+    return dp[0][n - 1] >= 0;
+  }
+};
+int main() {
+  Solution s;
+  cout << "" << "\n";
+  return 0;
+}
